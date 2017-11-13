bd75d1caa0e6434560be89fc6fed5adc78fa3ca4
Admin <soderstromshane@gmail.com>
2017-11-13 15:46:33 -0700

    Cleaned up posts index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-User.create!(
+@user = User.create!(
 	email: "test@test.com",
 	first_name: "Testing",
 	last_name: "Tester",
@@ -11,6 +11,6 @@ puts "Created one user named 'Testing Tester'"
 
 100.times do |post|
 	
-	Post.create!(date: Date.today, rationale: "#{post} rationale content")
+	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
 puts "100 posts created"
