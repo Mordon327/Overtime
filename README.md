cfaa0e1ece52ca44fd39689eaaf3c40dcc67c2ef
Admin <soderstromshane@gmail.com>
2017-11-20 13:49:07 -0700

    added pundit, blocked nav bar from guest users and customized admin behavior.

--- README.md
+++ README.md
@@ -21,3 +21,4 @@
 
 ## Refactor TODOS:
 - Refactor user association integration test in post_spec
+- Refactor posts/_form for admin
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,13 @@
 	first_name: "Testing",
 	last_name: "Tester",
 	password: "asdfasdf",
-	password_confirmation: "asdfasdf"
+	password_confirmation: "asdfasdf",
 	)
 
 puts "Created one user named 'Testing Tester'"
 
+AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Admin", last_name: "Name")
+puts "Created one admin user named Admin Name"
 
 100.times do |post|
 	
