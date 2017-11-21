e5a1cb0155bf26e441e2c213b71ecfc2966fdcbe
Admin <soderstromshane@gmail.com>
2017-11-21 15:33:10 -0700

    added phone to users and made it required

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,15 +4,28 @@
 	last_name: "Tester",
 	password: "asdfasdf",
 	password_confirmation: "asdfasdf",
+	phone: "801-857-4399",
 	)
 
 puts "Created one user named 'Testing Tester'"
 
-AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Admin", last_name: "Name")
+AdminUser.create(
+		email: "admin@test.com", 
+		password: "asdfasdf", 
+		password_confirmation: "asdfasdf", 
+		first_name: "Admin", 
+		last_name: "Name",
+		phone: "801-857-4399",
+		)
 puts "Created one admin user named Admin Name"
 
 100.times do |post|
 	
-	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+	Post.create!(
+		date: Date.today, 
+		rationale: "#{post} rationale content", 
+		user_id: @user.id, 
+		overtime_request: 2.5,
+		)
 end
 puts "100 posts created"
