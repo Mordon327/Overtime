0b050da8edf871a58776d2527bc6129d55b7c758
Admin <soderstromshane@gmail.com>
2017-11-30 16:03:25 -0700

    implemented functional buttons for employee homepage confirmations

--- app/controllers/audit_logs_controller.rb
+++ app/controllers/audit_logs_controller.rb
@@ -7,4 +7,11 @@ class AuditLogsController < ApplicationController
 	def edit
 		
 	end
+
+	def confirm
+		audit_log = AuditLog.find(params[:id])
+		authorize audit_log
+		audit_log.confirmed!
+		redirect_to root_path, notice: "Thank you, your confirmation has been successfully made."
+	end
 end
